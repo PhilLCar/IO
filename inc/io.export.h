e569f5b30baf01a9a99e7ca44c59fab70ef8bb13
Philippe Caron <philippe-caron@hotmail.com>
2025-08-23 15:12:04 -0400

    remove dependency on __VA_OPT__

--- inc/io.export.h
+++ inc/io.export.h
@@ -8,9 +8,6 @@
 #else
 #define IO_EXPORT __declspec(dllimport)
 #endif
-#ifdef __INTELLISENSE__
-#define __VA_OPT__(X)
-#endif
 #else
 #define IO_EXPORT __attribute__((visibility("default")))
 #endif
